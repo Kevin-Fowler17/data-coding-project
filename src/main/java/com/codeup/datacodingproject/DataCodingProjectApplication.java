ddecea89156a74d67312a6535467615701a162f2
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-06-20 15:06:13 -0500

    initial commit part ii

---
+++ src/main/java/com/codeup/datacodingproject/DataCodingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.datacodingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCodingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCodingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/datacodingproject/DataCodingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.datacodingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCodingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
