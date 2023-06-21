25c15b5de7aaf76fac89cfff559f1aeae7f4073e
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-06-21 13:08:48 -0500

    main table setup

---
+++ src/main/java/com/codeup/datacodingproject/models/Main.java
@@ -0,0 +1,70 @@
+package com.codeup.datacodingproject.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "main")
+public class Main {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "INT(10) UNSIGNED")
+    private long id;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q1;
+
+    @Column(nullable = false)
+    private String q2;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_1;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_2;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_3;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_4;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_5;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_6;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_7;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_8;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_9;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q4_10;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q5;
+
+    @Column(nullable = false)
+    private String q6;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q8;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q9;
+
+    @Column(nullable = false, columnDefinition = "INT(10)")
+    private long q10;
+}
