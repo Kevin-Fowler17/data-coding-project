4435e26f19575b24f889e53898ae897199a4e81f
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-07-14 14:00:46 -0500

    working on generating q10 with javascript

--- src/main/resources/static/js/generateSurveyQuestions.js
+++ src/main/resources/static/js/generateSurveyQuestions.js
@@ -4,11 +4,18 @@
 
     // const q1 = document.getElementById("q1");
     ``
-    const q10 = document.getElementById("q10")
+    // const displayQ10 = document.getElementById("q10");
+    const displayQ10 = document.getElementsByClassName("q10")[0];
+
+    renderQ10()
 
     function renderQ10() {
 
         let questionNumber = 10;
+
+        console.log("***********************")
+        console.log("It got in.")
+        console.log("***********************")
 
         let surveyQuestion = document.createElement("div");
         let label = document.createElement("label");
@@ -37,88 +44,134 @@
 
         surveyQuestion.className = "survey-question";
 
-        label.className = "question-text";
-        label.innerHTML = "Q" + questionNumber + ". What is your income?"
+        label.className = "pb-2 pt-3"
+        label.setAttribute("for", "radio-group");
+        label.innerHTML = "Q10. What is your income?";
 
         divRadioGroup.className = "";
-        divRadioGroup.setAttribute("id", "question-group" + questionNumber);
+        divRadioGroup.setAttribute("id", "radio-group")
 
         divFormCheck1.className = "form-check pb-2";
 
-        for (let i = 0; i < incomeArray.length; i++) {
-            return 'input' + (i + 1) + '.setAttribute("type", "radio");' +
-            'input' + (i + 1) + '.className = "form-check-input";' +
-            'input' + (i + 1) + '.setAttribute("id", "radio_" + questionNumber + "_1");' +
-            'input' + (i + 1) + '.setAttribute("name", "q" + questionNumber)' +
-            'input' + (i + 1) + '.setAttribute("value", "1");'
+        input1.setAttribute("type", "radio");
+        input1.className = "form-check-input";
+        input1.setAttribute("id", "radio_" + questionNumber + "_0");
+        input1.setAttribute("name", "q" + questionNumber)
+        input1.setAttribute("value", "a");
 
-        }
+        label1.className = "form-check-label";
+        label1.setAttribute("for", "radio_" + questionNumber + "_0");
+        label1.innerHTML = incomeArray[0].label;
 
-        // input1.setAttribute("type", "radio");
-        // input1.className = "form-check-input";
-        // input1.setAttribute("id", "radio_" + questionNumber + "_1");
-        // input1.setAttribute("name", "q" + questionNumber)
-        // input1.setAttribute("value", "1");
-        //
-        // label1.className = "form-check-label";
-        // label1.setAttribute("for", "radio_" + index + "_0");
-        // label1.innerHTML = quiz.answers.a;
+        divFormCheck2.className = "form-check pb-2";
 
+        input2.setAttribute("type", "radio");
+        input2.className = "form-check-input";
+        input2.setAttribute("id", "radio_" + questionNumber + "_0");
+        input2.setAttribute("name", "q" + questionNumber)
+        input2.setAttribute("value", "a");
+
+        label2.className = "form-check-label";
+        label2.setAttribute("for", "radio_" + questionNumber + "_0");
+        label2.innerHTML = incomeArray[1].label;
+
+        divFormCheck3.className = "form-check pb-2";
+
+        input3.setAttribute("type", "radio");
+        input3.className = "form-check-input";
+        input3.setAttribute("id", "radio_" + questionNumber + "_0");
+        input3.setAttribute("name", "q" + questionNumber)
+        input3.setAttribute("value", "a");
+
+        label3.className = "form-check-label";
+        label3.setAttribute("for", "radio_" + questionNumber + "_0");
+        label3.innerHTML = incomeArray[2].label;
+
+        divFormCheck4.className = "form-check pb-2";
+
+        input4.setAttribute("type", "radio");
+        input4.className = "form-check-input";
+        input4.setAttribute("id", "radio_" + questionNumber + "_0");
+        input4.setAttribute("name", "q" + questionNumber)
+        input4.setAttribute("value", "a");
+
+        label4.className = "form-check-label";
+        label4.setAttribute("for", "radio_" + questionNumber + "_0");
+        label4.innerHTML = incomeArray[3].label;
+
+        divFormCheck5.className = "form-check pb-2";
+
+        input5.setAttribute("type", "radio");
+        input5.className = "form-check-input";
+        input5.setAttribute("id", "radio_" + questionNumber + "_0");
+        input5.setAttribute("name", "q" + questionNumber)
+        input5.setAttribute("value", "a");
+
+        label5.className = "form-check-label";
+        label5.setAttribute("for", "radio_" + questionNumber + "_0");
+        label5.innerHTML = incomeArray[4].label;
+
+        divFormCheck6.className = "form-check pb-2";
+
+        input6.setAttribute("type", "radio");
+        input6.className = "form-check-input";
+        input6.setAttribute("id", "radio_" + questionNumber + "_0");
+        input6.setAttribute("name", "q" + questionNumber)
+        input6.setAttribute("value", "a");
+
+        label6.className = "form-check-label";
+        label6.setAttribute("for", "radio_" + questionNumber + "_0");
+        label6.innerHTML = incomeArray[5].label;
+
+        divFormCheck99.className = "form-check pb-2";
+
+        input99.setAttribute("type", "radio");
+        input99.className = "form-check-input";
+        input99.setAttribute("id", "radio_" + questionNumber + "_0");
+        input99.setAttribute("name", "q" + questionNumber)
+        input99.setAttribute("value", "a");
+
+        label99.className = "form-check-label";
+        label99.setAttribute("for", "radio_" + questionNumber + "_0");
+        label99.innerHTML = incomeArray[6].label;
 
         divFormCheck1.appendChild(input1);
         divFormCheck1.appendChild(label1);
 
-        // divFormCheck2.appendChild(input2);
-        // divFormCheck2.appendChild(label2);
-        //
-        // divFormCheck3.appendChild(input3);
-        // divFormCheck3.appendChild(label3);
-        //
-        // divFormCheck4.appendChild(input4);
-        // divFormCheck4.appendChild(label4);
+        divFormCheck2.appendChild(input2);
+        divFormCheck2.appendChild(label2);
+
+        divFormCheck3.appendChild(input3);
+        divFormCheck3.appendChild(label3);
+
+        divFormCheck4.appendChild(input4);
+        divFormCheck4.appendChild(label4);
+
+        divFormCheck4.appendChild(input5);
+        divFormCheck4.appendChild(label5);
+
+        divFormCheck4.appendChild(input6);
+        divFormCheck4.appendChild(label6);
+
+        divFormCheck4.appendChild(input99);
+        divFormCheck4.appendChild(label99);
 
         divRadioGroup.appendChild(divFormCheck1);
-        // divRadioGroup.appendChild(divFormCheck2);
-        // divRadioGroup.appendChild(divFormCheck3);
-        // divRadioGroup.appendChild(divFormCheck4);
+        divRadioGroup.appendChild(divFormCheck2);
+        divRadioGroup.appendChild(divFormCheck3);
+        divRadioGroup.appendChild(divFormCheck4);
+        divRadioGroup.appendChild(divFormCheck5);
+        divRadioGroup.appendChild(divFormCheck6);
+        divRadioGroup.appendChild(divFormCheck99);
 
         surveyQuestion.appendChild(label);
 
         surveyQuestion.appendChild(divRadioGroup);
 
-        // displayQuiz.appendChild(quizQuestion);
+        displayQ10.appendChild(surveyQuestion);
 
-
-
-
-        incomeArray.forEach(renderRadioOptions);
-
-
-
-
-        // <p>Q10. What is your income?</p>
-        // <label><input type="radio" name="q10" value="1"> Less than $20,000</label><br>
-        // <label><input type="radio" name="q10" value="2"> $20,000 to $34,999</label><br>
-        // <label><input type="radio" name="q10" value="3"> $35,000 to $49,999</label><br>
-        // <label><input type="radio" name="q10" value="4"> $50,000 to $74,999</label><br>
-        // <label><input type="radio" name="q10" value="5"> $75,000 to $99,999</label><br>
-        // <label><input type="radio" name="q10" value="6"> $100,000 or more</label><br>
-        // <label><input type="radio" name="q10" value="99"> Refused</label><br>
-
-
-    }
-
-    function renderRadioOptions(incomeArray, index) {
-
-        input1.setAttribute("type", "radio");
-        input1.className = "form-check-input";
-        input1.setAttribute("id", "radio_" + index + "_0");
-        input1.setAttribute("name", "q" + index)
-        input1.setAttribute("value", "a");
-
-        label1.className = "form-check-label";
-        label1.setAttribute("for", "radio_" + index + "_0");
-        label1.innerHTML = quiz.answers.a;
+        console.log(incomeArray);
+        console.log(displayQ10);
 
     }
 
@@ -130,9 +183,6 @@
         {'value': 5, 'label': '$75,000 to $99,999'},
         {'value': 6, 'label': '$100,000 or more'},
         {'value': 99, 'label': 'Refused'},
-    ]
-
-
-
+    ];
 
 })();
