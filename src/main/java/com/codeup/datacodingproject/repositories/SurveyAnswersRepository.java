b269251a5919e58007205e90611b376b4f2d21c7
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-08-07 16:05:44 -0500

    labels are now being outputted instead of values

--- src/main/java/com/codeup/datacodingproject/repositories/SurveyAnswersRepository.java
+++ src/main/java/com/codeup/datacodingproject/repositories/SurveyAnswersRepository.java
@@ -10,7 +10,16 @@ import java.util.List;
 
 public interface SurveyAnswersRepository extends JpaRepository<SurveyAnswers, Long> {
 
-   @Query(value = "SELECT q1.q1, ROUND((COUNT(q1.q1) / (SELECT COUNT(*) FROM q1)) * 100, 0) AS percentage FROM q1 GROUP BY q1.q1", nativeQuery = true)
+   // percentages without labels
+//   @Query(value = "SELECT q1.q1, ROUND((COUNT(q1.q1) / (SELECT COUNT(*) FROM q1)) * 100, 0) AS percentage FROM q1 GROUP BY q1.q1", nativeQuery = true)
+//   List<Object[]> getQ1AnswerPercentages();
+
+   // percentages with labels
+   @Query(value = "SELECT q1_labels.name, ROUND((COUNT(q1.q1) / (SELECT COUNT(*) FROM survey_answers)) * 100, 0) AS percentage " +
+           "FROM q1 " +
+           "JOIN q1_labels ON q1.q1 = q1_labels.id " +
+           "GROUP BY q1_labels.name", nativeQuery = true)
    List<Object[]> getQ1AnswerPercentages();
+
 
 }
