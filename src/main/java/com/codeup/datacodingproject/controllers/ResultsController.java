a86af92cf42618e40efbfc1ba92081fe227ca6cd
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-08-08 14:38:52 -0500

    added sr and mr questions to the results page

--- src/main/java/com/codeup/datacodingproject/controllers/ResultsController.java
+++ src/main/java/com/codeup/datacodingproject/controllers/ResultsController.java
@@ -19,22 +19,22 @@ public class ResultsController {
     @GetMapping("/results")
     public String showResultsForm(Model model){
 
-        System.out.println("**********************");
-        System.out.println("Results Controller");
-        System.out.println("**********************");
+//        System.out.println("**********************");
+//        System.out.println("Results Controller");
+//        System.out.println("**********************");
 
         List<Object[]> q1Percentages = surveyAnswersDao.getQ1AnswerPercentages();
 
         model.addAttribute("q1Percentages", q1Percentages);
 
-        System.out.println("**************************");
-        for (Object[] row : q1Percentages) {
-            for (Object value : row) {
-                System.out.print(value + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("**************************");
+//        System.out.println("**************************");
+//        for (Object[] row : q1Percentages) {
+//            for (Object value : row) {
+//                System.out.print(value + " ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("**************************");
 
 
         return "results";
