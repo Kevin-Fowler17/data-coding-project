7c889d18faac56597e056382bf8726117455f6a2
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-08-09 15:14:18 -0500

    working on getting the query down to one

--- src/main/java/com/codeup/datacodingproject/controllers/ResultsController.java
+++ src/main/java/com/codeup/datacodingproject/controllers/ResultsController.java
@@ -23,20 +23,21 @@ public class ResultsController {
 //        System.out.println("Results Controller");
 //        System.out.println("**********************");
 
-        List<Object[]> q1Percentages = surveyAnswersDao.getQ1AnswerPercentages();
-        List<Object[]> q3Percentages = surveyAnswersDao.getQ3AnswerPercentages();
-        List<Object[]> q5Percentages = surveyAnswersDao.getQ5AnswerPercentages();
-        List<Object[]> q7Percentages = surveyAnswersDao.getQ7AnswerPercentages();
-        List<Object[]> q9Percentages = surveyAnswersDao.getQ9AnswerPercentages();
-        List<Object[]> q10Percentages = surveyAnswersDao.getQ10AnswerPercentages();
+        List<Object[]> q1AnswerPercentages = surveyAnswersDao.getQAnswerPercentages(1);
+        List<Object[]> q3AnswerPercentages = surveyAnswersDao.getQAnswerPercentages(3);
+        List<Object[]> q5AnswerPercentages = surveyAnswersDao.getQAnswerPercentages(5);
+        List<Object[]> q7AnswerPercentages = surveyAnswersDao.getQAnswerPercentages(7);
+        List<Object[]> q9AnswerPercentages = surveyAnswersDao.getQAnswerPercentages(9);
+        List<Object[]> q10AnswerPercentages = surveyAnswersDao.getQAnswerPercentages(10);
 
 
-        model.addAttribute("q1Percentages", q1Percentages);
-        model.addAttribute("q3Percentages", q3Percentages);
-        model.addAttribute("q5Percentages", q5Percentages);
-        model.addAttribute("q7Percentages", q7Percentages);
-        model.addAttribute("q9Percentages", q9Percentages);
-        model.addAttribute("q10Percentages", q10Percentages);
+
+        model.addAttribute("q1AnswerPercentages", q1AnswerPercentages);
+        model.addAttribute("q3AnswerPercentages", q3AnswerPercentages);
+        model.addAttribute("q5AnswerPercentages", q5AnswerPercentages);
+        model.addAttribute("q7AnswerPercentages", q7AnswerPercentages);
+        model.addAttribute("q9AnswerPercentages", q9AnswerPercentages);
+        model.addAttribute("q10AnswerPercentages", q10AnswerPercentages);
 
 
 //        System.out.println("**************************");
